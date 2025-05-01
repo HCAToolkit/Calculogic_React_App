0e153aeca46280c0b2cc164966f9757683cb7d5e
Yuri-bara <143827071+Yuri-bara@users.noreply.github.com>
2025-05-01 01:08:14 +0000

    chore: Removed React Demo Scaffolding

--- my-app/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx'; // Explicitly includes the .tsx extension
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
